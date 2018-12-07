101eee49ac4d63bbcc845c86120587fc68b138e9
realamiel <francisamiel.real.iics@ust.edu.ph>
2018-12-07 18:54:21 +0800

    List search added

--- app/src/main/java/com/real/francisamiel/diseaseawarenessapp/SymptomsSearch.java
+++ app/src/main/java/com/real/francisamiel/diseaseawarenessapp/SymptomsSearch.java
@@ -1,13 +1,90 @@
 package com.real.francisamiel.diseaseawarenessapp;
 
+import android.content.Intent;
+import android.graphics.ColorSpace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SymptomsSearch extends AppCompatActivity {
 
+    ArrayList<String> myArrayList = new ArrayList<>();
+    Firebase myFirebase;
+    private ArrayAdapter<String> myArrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symptoms_search);
+        Firebase.setAndroidContext(this);
+        myFirebase = new Firebase("https://diseaseawarenessapp.firebaseio.com/");
+        ListView list = findViewById(R.id.theList);
+        EditText filter = findViewById(R.id.theFilter);
+        myArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,myArrayList);
+        list.setAdapter(myArrayAdapter);
+
+        myFirebase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String myChildValues = dataSnapshot.getValue(String.class);
+                myArrayList.add(myChildValues);
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        filter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                (SymptomsSearch.this).myArrayAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
+
+
+
 }
